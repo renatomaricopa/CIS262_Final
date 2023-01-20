9760b6068cbbe069ea2ad1a9022f7d3f727a2a24
Renato Patron <REN2047506@maricopa.edu>
2023-01-19 20:49:03 -0700

    Fixed text in the web pages, and corrected problem with the server not loading boottrtrap or other sccripts in the www folder.

---
+++ HighSchoolApplication/Pages/Student/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HighSchoolApplication.Pages.Student
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
