1ac2c8c66b84239d3c4c7f147344838f1eb6eb39
Renato Patron <REN2047506@maricopa.edu>
2023-01-25 09:46:17 -0700

    Last version as 1/25

--- HighSchoolApplication/Pages/Exam/Edit.cshtml.cs
+++ HighSchoolApplication/Pages/Exam/Edit.cshtml.cs
@@ -1,12 +1,83 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+using HighSchoolClasses.DAL;
 
 namespace HighSchoolApplication.Pages.Exam
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private IExamAdapter _examAdapter;
+        public EditModel(IExamAdapter examAdapter)
         {
+            _examAdapter = examAdapter;
+        }
+
+        [BindProperty]
+        [Required]
+        public string FirstName { get; set; }
+        [BindProperty]
+        [Required]
+        public string LastName { get; set; }
+        public bool IsSuccess { get; set; }
+        public List<SelectListItem> Exam { get; set; }
+        [BindProperty]
+        public int ExamId { get; set; }
+
+
+        public void OnGet(int id = 0)
+        {
+            if (id != 0)
+            {
+                HighSchoolClasses.DAL.Exam exam = _examAdapter.GetExamById(id);
+                if (exam != null)
+                {
+                    FirstName = exam.FirstName;
+                    LastName = exam.LastName;
+                    ExamId = exam.StudentId;
+                }
+            }
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                HighSchoolClasses.DAL.Exam exam = new HighSchoolClasses.DAL.Exam();
+                exam.FirstName = FirstName;
+                exam.LastName = LastName;
+                exam.ExamId = ExamId;
+
+                if (ExamId > 0)
+                {
+                    bool isUpdate = _examAdapter.UpdateExam(exam);
+                    if (isUpdate)
+                    {
+                        IsSuccess = true;
+                    }
+                    else
+                    {
+                        IsSuccess = false;
+                    }
+                }
+                else
+                {
+                    bool isCreate = _examAdapter.InsertExam(exam);
+                    if (isCreate)
+                    {
+                        IsSuccess = true;
+                    }
+                    else
+                    {
+                        IsSuccess = false;
+                    }
+                }
+            }
+            else
+            {
+                IsSuccess = false;
+            }
         }
     }
 }
--- HighSchoolApplication/Pages/Exam/Index.cshtml.cs
+++ HighSchoolApplication/Pages/Exam/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using HighSchoolClasses.DAL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace HighSchoolApplication.Pages.Exam
 {
     public class IndexModel : PageModel
     {
+        private readonly IExamAdapter _examAdapter;
+        public IndexModel(IExamAdapter examAdapter)
+        {
+            _examAdapter = examAdapter;
+        }
+        public IEnumerable<HighSchoolClasses.DAL.Exam> Exams { get; set; }
+
+        // Variables declaration
+        [BindProperty]
+        public string FirstName { get; set; }
+        [BindProperty]
+        public string LastName { get; set; }
+        public bool IsSuccess { get; set; }
+
+
         public void OnGet()
         {
+                Exams = _examAdapter.GetAllExams(); 
+        }
+
+        // OnPost
+        public void OnPost()
+        {
+            // IsSuccess = true;
         }
     }
 }
